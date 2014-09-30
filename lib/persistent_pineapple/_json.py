b9b25d989bf6b9a1056c112f713840da02b65c59
Timothy McFadden <tim@timandjamie.com>
2014-09-30 11:20:35 -0600

    initial try at keeping comments

--- lib/persistent_pineapple/_json.py
+++ lib/persistent_pineapple/_json.py
@@ -17,6 +17,7 @@ import json
 import re
 import sys
 
+
 ###############################################################################
 def container_to_ascii(item):
     '''Converts all items from unicode to ascii, where needed.  This is a
@@ -35,7 +36,7 @@ def container_to_ascii(item):
         >>>
     '''
     result = None
-    
+
     if sys.version_info[0] == 2 and type(item) is unicode:
         result = item.encode('ascii')
     elif type(item) is list:
@@ -65,7 +66,7 @@ class JSON(object):
 
     JSON reads much like Python; [] is for a list of items, {} is for a
     dictionary.
-    
+
     Some important notes:
 
     * single-quotes (B{'}) are invalid; always use double-quotes (B{"})
@@ -77,17 +78,17 @@ class JSON(object):
     * JSON does not supports comments.  This functionality was added to this object to make life easier.
 
     We support the following types of comments
-    
+
     * A line consisting of 0 or more whitespace characters followed by ``//``
     * Text ending a line with 1 or more whitespace characters followed
       by ``//``
     * All text in between ``/*`` and ``*/``
 
     Examples
-    
+
         .. code-block:: python
             :linenos:
-            
+
             // this line is ignored
             {"key": 5} // the rest of the line is ignored
             {"key": 5 /*,"key2": 6*/}  // key2 and the remainder is ignored
@@ -163,7 +164,7 @@ class JSON(object):
                 if ("Expecting property name" in err.message) and \
                    ("'" in context):
                     print("Possible invalid single-quote around here (JSON "
-                        "only supports double-quotes):")
+                          "only supports double-quotes):")
                     print(context)
                 elif "Expecting property name" in err.message:
                     print("Possible trailing comma somewhere around here:")
@@ -192,3 +193,171 @@ def encode(data, cls=MyEncoder):
     '''encode the provided data in JSON format'''
     return json.dumps(data, sort_keys=True, indent=4, separators=(',', ': '),
                       cls=cls)
+
+
+class JSONComment(object):
+    '''A simple object to contain information about a comment found in a JSON
+    file/string.'''
+    def __init__(self, _type, line_number, comment, setting=None):
+        self._type = _type  # "line"  # "hanging", "inline"
+        self._line_number = line_number
+        self._comment = comment
+        self._setting_name = setting
+
+
+class CommentedJSON(JSON):
+    '''This object is used to _approximately_ keep the comments in the stored
+    text file.  The order and format of the original file **is not kept**.  The
+    data is translated to Python native objects.  In the case of a dictionary,
+    key order is not guaranteed.  This object does not attempt to re-order the
+    output.
+
+    **Known Issues**
+
+    - Line comments not immediately preceding a setting are ignored
+    - ``/*  */`` style comments are discarded
+    - Block comments not associated with a setting are ignored, with the exception
+      of a comment at the top of the file (called the _header_)
+    - Settings with the same name will collide
+    - Guaranteed not to work well with complex JSON
+
+    **TODO**
+
+    Find/write a proper lexer
+    '''
+    RE_ANY_COMMENT = re.compile(r'((^\s*//|\s+//).*?$)|(/\*.*?\*/)')
+    RE_LINE_COMMENT = re.compile("^(\s*//.*)")
+    RE_HANING_COMMENT = re.compile('^\s*["{}\[\]0-9].*(// .*)$')
+
+    def __init__(self):
+        super(CommentedJSON, self).__init__()
+
+        self._comments = []
+        self._header = []
+
+    def _setting_name(self, line):
+        '''Returns the name of a setting found in the line (the first item found
+        that is surrounded by double-quotes).
+        '''
+        if CommentedJSON.RE_ANY_COMMENT.match(line):
+            return
+        elif re.match("^\s*\"(.*?)\"", line):
+            return re.match("^\s*\"(.*?)\"", line).group(1)
+        elif re.match("^\s*(.*?),", line):
+            return re.match("^\s*(.*?),", line).group(1)
+
+        return
+
+    def _setting_line_comment(self, line):
+        '''Returns any line comment for the given setting found in the line.'''
+        setting = self._setting_name(line)
+        if not setting:
+            return
+
+        comments = [x for x in self._comments if (
+            x._setting_name and
+            (setting == x._setting_name) and
+            (x._type == "line"))]
+
+        if comments:
+            return comments[0]._comment
+
+        return
+
+    def _setting_hanging_comment(self, line):
+        '''Returns any hanging comment for the given setting found in the
+        line (non-comment followed by //*).'''
+        setting = self._setting_name(line)
+        if not setting:
+            return
+
+        comments = [x for x in self._comments if (
+            x._setting_name and
+            (setting == x._setting_name) and
+            (x._type == "hanging"))]
+
+        if comments:
+            return comments[0]._comment
+
+        return
+
+    def load(self, string=None, path=None, force_ascii=True):
+        '''Loads JSON data from either a string or a file.  The _template_
+        of the file will be used during as_string() and store().
+
+        :param basestring string: The string to parse
+        :param basestring path: The path of the file to parse
+        :param bool force_ascii: For lazy people who refuse to use unicode; the
+            data returned is guaranteed to be ascii.
+
+        :returns: JSON encoded data
+        '''
+        json_data = super(CommentedJSON, self).load(string, path, force_ascii)
+
+        if string:
+            lines = string.split("\n")
+        elif path:
+            with open(path, 'rb') as pfile:
+                text = pfile.read().decode()
+            lines = text.split("\n")
+        else:
+            raise ValueError("Must enter either string or path")
+
+        header = True
+        for index, line in enumerate(lines):
+            line = line.rstrip()
+            if CommentedJSON.RE_LINE_COMMENT.match(line):
+                comment = CommentedJSON.RE_LINE_COMMENT.match(line).group(1)
+                obj = JSONComment("line", index, comment)
+                if self._setting_name(lines[index + 1]):
+                    obj._setting_name = self._setting_name(lines[index + 1])
+
+                if header:
+                    self._header.append(comment)
+                else:
+                    self._comments.append(obj)
+            elif CommentedJSON.RE_HANING_COMMENT.match(line):
+                comment = CommentedJSON.RE_HANING_COMMENT.match(line).group(1)
+                setting = self._setting_name(line)
+
+                obj = JSONComment("hanging", index, comment, setting)
+                self._comments.append(obj)
+                header = False
+            else:
+                header = False
+
+        return json_data
+
+    def as_string(self, data, cls=MyEncoder):
+        string = encode(data, cls)
+
+        if self._header:
+            result = "\n".join(self._header)
+            result += "\n"
+        else:
+            result = ""
+
+        lines = string.split("\n")
+
+        for index, line in enumerate(lines):
+            if self._setting_line_comment(line):
+                result += "\n{}\n{}\n".format(
+                    self._setting_line_comment(line), line)
+            elif self._setting_hanging_comment(line):
+                result += "{}  {}\n".format(
+                    line, self._setting_hanging_comment(line).strip())
+            else:
+                result += line
+                result += "\n"
+
+        return result
+
+    def store(self, data, path, cls=MyEncoder):
+        '''Write the data to a JSON formatted file while attempting to keep
+        comments.'''
+        if sys.version_info[0] == 3:
+            bytedata = bytes(self.as_string(data, cls), 'UTF-8')
+        else:
+            bytedata = self.as_string(data, cls)
+        with open(path, 'wb') as pfile:
+            pfile.write(bytedata)
---
+++ lib/tests/test_comment.py
@@ -0,0 +1,85 @@
+#!/usr/bin/env python
+
+__author__ = "Timothy McFadden"
+__copyright__ = "Copyright 2014"
+__credits__ = ["Timothy McFadden", "Jason Unrein"]
+__license__ = "GPL"
+__version__ = "0.0.0.1"  # file version
+__maintainer__ = "Jason Unrein"
+__email__ = "JasonAUnrein@gmail.com"
+__status__ = "Development"
+
+# Imports #####################################################################
+import re
+import unittest
+from os import path, unlink
+from persistent_pineapple._json import CommentedJSON
+
+
+###############################################################################
+class CommentTest(unittest.TestCase):
+    test_path = path.dirname(path.realpath(__file__))
+    test_file = "test1.json"
+    fqtest = path.join(test_path, test_file)
+    fqlist = path.join(test_path, "list.json")
+    fqcomment = path.join(test_path, "test-comment.json")
+
+    @classmethod
+    def tearDownClass(cls):
+        if path.isfile(CommentTest.fqcomment):
+            unlink(CommentTest.fqcomment)
+
+    def test_load(self):
+        CommentedJSON().load(path=CommentTest.fqtest)
+
+    def test_string(self):
+        obj = CommentedJSON()
+        data = obj.load(path=CommentTest.fqtest)
+        result = obj.as_string(data)
+
+        self.assertIsInstance(result, basestring)
+
+        with open(self.fqtest, 'rb') as fh:
+            lines = list(fh)[:2]
+
+        lines = [x.strip() for x in lines]
+        header = result.split("\n")[:2]
+
+        self.assertEqual(lines, header)
+
+        self.assertTrue(re.search("a setting's comment\n\s+\"setting1", result))
+        self.assertTrue(re.search("\"setting3\".*// inline", result))
+
+    def test_list(self):
+        obj = CommentedJSON()
+        data = obj.load(path=CommentTest.fqlist)
+        result = obj.as_string(data)
+
+        self.assertTrue(re.search("2,\s+// hanging", result))
+
+    def test_store(self):
+        obj = CommentedJSON()
+        data = obj.load(path=CommentTest.fqtest)
+        result = obj.as_string(data)
+
+        obj.store(data=data, path=CommentTest.fqcomment)
+
+        with open(CommentTest.fqcomment, 'rb') as fh:
+            file_text = fh.read()
+
+        self.assertEqual(result, file_text)
+
+    def test_load_from_string(self):
+        string = '//heading line1\n//heading line2\n{\n    //test\n    "setting1": 2\n}'
+        obj = CommentedJSON()
+        data = obj.load(string=string)
+
+        self.assertEqual(data, {"setting1": 2})
+
+        as_string = obj.as_string(data)
+        self.assertEqual(string.split("\n")[:2], as_string.split("\n")[:2])
+
+
+###############################################################################
+if __name__ == "__main__":
+    unittest.main()
